b1848c45d1226fde03a6196059ba89b3a273f914
Redskull-127 <meerraja17@gmail.com>
2022-02-20 21:05:48 +0530

    Working

---
+++ script/indexscript.js
@@ -0,0 +1,8 @@
+function man() {
+  document.getElementById("light").style.display = "block";
+  document.getElementById("fade").style.display = "block";
+}
+function feman() {
+  document.getElementById("light").style.display = "none";
+  document.getElementById("fade").style.display = "none";
+}
