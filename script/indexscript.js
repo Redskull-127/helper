e2462fc5023e244fc6e0eeeadec01fcc65d675d2
Redskull-127 <meerraja17@gmail.com>
2022-02-21 21:28:37 +0530

    Starting layout

--- script/indexscript.js
+++
@@ -1,8 +0,0 @@
-function man() {
-  document.getElementById("light").style.display = "block";
-  document.getElementById("fade").style.display = "block";
-}
-function feman() {
-  document.getElementById("light").style.display = "none";
-  document.getElementById("fade").style.display = "none";
-}
